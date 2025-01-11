2b364ab95ece779ec10fac52659c7aed9f43bb26
pspkurara <itinomiyakurara@yahoo.co.jp>
2025-01-12 06:35:31 +0900

    メニュー名の変更と、全変換の機能を追加

--- Assets/XWearAvatarFixer/Editor/AvatarFixerMenu.cs
+++ Assets/XWearAvatarFixer/Editor/AvatarFixerMenu.cs
@@ -16,16 +16,20 @@ namespace pspkurara.VRM10FromXRoidAvatarFixer.Editor
 
 		const string LastExportPathKey = "AVATAR FIXER LAST EXPORT PATH";
 
+		const string AssetContext = "Assets/";
+		const string GameObjectContext = "GameObject/";
 		const string DialogTitleContext = "Avatar Fixer";
 		const string MenuItemContextHeader = "XWear Avatar Fixer/";
 		const string CreateXWearSourceMenuItemContext = "Create/" + MenuItemContextHeader + "XWearSource";
 		const string FromXRoidAvatarFixingContext = MenuItemContextHeader + "From XRoid Avatar Fixing";
+		const string RemoveUnusedSpringBonesCollidersTransformContext = MenuItemContextHeader + "Remove Unused Spring Bones, Colliders, Transforms";
+		const string Optimization = MenuItemContextHeader + "Optimization";
 
 		/// <summary>
 		/// XRoidから書き出したVRM1.0ファイルを修復して書き出す
 		/// </summary>
-		[MenuItem("Assets/"+ FromXRoidAvatarFixingContext)]
-		public static void FixAvatar()
+		[MenuItem(AssetContext + FromXRoidAvatarFixingContext)]
+		public static void FixAndExportAvatar()
 		{
 
 			// 複数まとめて変換可能
@@ -89,8 +93,8 @@ namespace pspkurara.VRM10FromXRoidAvatarFixer.Editor
 		/// ヒエラルキー上にインスタンスされている状態でなければ動作しない
 		/// 形式がVRM1.0と同じであれば編集中のプレハブでも動作する筈
 		/// </summary>
-		[MenuItem("GameObject/"+ FromXRoidAvatarFixingContext)]
-		public static void FixAndExportAvatar()
+		[MenuItem(GameObjectContext + FromXRoidAvatarFixingContext)]
+		public static void FixAvatar()
 		{
 			var selectingAvatarRoot = Selection.objects
 				.Cast<GameObject>()
@@ -119,8 +123,8 @@ namespace pspkurara.VRM10FromXRoidAvatarFixer.Editor
 		/// ヒエラルキー上にインスタンスされている状態でなければ動作しない
 		/// 形式がVRM1.0と同じであれば編集中のプレハブでも動作する筈
 		/// </summary>
-		[MenuItem("GameObject/" + MenuItemContextHeader + "Remove Unused Spring Bones And Colliders")]
-		public static void RemoveUnusedSpringBoneAndColliders()
+		[MenuItem(GameObjectContext + RemoveUnusedSpringBonesCollidersTransformContext)]
+		public static void RemoveUnusedSpringBoneCollidersTransforms()
 		{
 			var selectingAvatarRoot = Selection.objects
 				.Cast<GameObject>()
@@ -142,9 +146,108 @@ namespace pspkurara.VRM10FromXRoidAvatarFixer.Editor
 		}
 
 		/// <summary>
+		/// XRoidから書き出したVRM1.0ファイルを修復して書き出す
+		/// その他の全修正を挟む
+		/// </summary>
+		[MenuItem(AssetContext + Optimization)]
+		public static void OptimizationAndExportAvatar()
+		{
+			// 複数まとめて変換可能
+			var selectingAvatarRoot = Selection.objects
+				.Where(o => o != null);
+
+			// 1つも選んでいなかったらやめる
+			if (selectingAvatarRoot.Count() == 0) return;
+
+			// XWearの元データがないとだめ
+			if (CheckNotExsitsXWearSourceDialog()) return;
+
+			// エクスポート先を選ぶ
+			// 選んだフォルダにまとめて出力されるので注意
+			var path = GetDir("Export Directory");
+			if (string.IsNullOrEmpty(path)) return;
+
+			Debug.Log("Start Optimize.");
+
+			try
+			{
+
+				float count = 1f / selectingAvatarRoot.Count();
+				float currentCount = 0;
+				foreach (var o in selectingAvatarRoot)
+				{
+					// 1つずつエクスポートする
+					if (EditorUtility.DisplayCancelableProgressBar("Exporting...", o.name, currentCount))
+					{
+						return;
+					}
+					Vrm10Export export = new Vrm10Export(o as GameObject);
+					// 保存前に修復処理を行う
+					export.ExportPreLogic = (go) =>
+					{
+						// 修復処理
+						var vrmInstance = go.GetComponent<Vrm10Instance>();
+						AvatarFixer.ConvertSpringBone(vrmInstance);
+						// 最適化処理
+						AvatarFixer.RemoveUnuseSpringBone(vrmInstance);
+					};
+					// 書き出し
+					export.Export(Path.Combine(path, $"{o.name}.vrm"));
+					Debug.Log($"Completed {o.name}");
+				}
+				Debug.Log("Optimize Completed.");
+				// 書き出し終えたのでダイアログを出してやる
+				EditorUtility.RevealInFinder(path);
+
+			}
+			catch (System.Exception e)
+			{
+				// 失敗したとき用に例外処理
+				Debug.LogException(e);
+			}
+			finally
+			{
+				EditorUtility.ClearProgressBar();
+			}
+		}
+
+		/// <summary>
+		/// XRoidから書き出したVRM1.0プレハブを修復する
+		/// その他の全修正を挟む
+		/// ヒエラルキー上にインスタンスされている状態でなければ動作しない
+		/// 形式がVRM1.0と同じであれば編集中のプレハブでも動作する筈
+		/// </summary>
+		[MenuItem(GameObjectContext + Optimization)]
+		public static void OptimizationAvatar()
+		{
+
+			var selectingAvatarRoot = Selection.objects
+				.Cast<GameObject>()
+				.Select(o => o.GetComponent<Vrm10Instance>())
+				.Where(o => o != null);
+
+			// 1つも選んでいなかったらやめる
+			if (selectingAvatarRoot.Count() == 0) return;
+
+			// XWearの元データがないとだめ
+			if (CheckNotExsitsXWearSourceDialog()) return;
+
+			Debug.Log("Start Optimize.");
+			foreach (var o in selectingAvatarRoot)
+			{
+				// 1つずつ変換する
+				AvatarFixer.ConvertSpringBone(o);
+				AvatarFixer.RemoveUnuseSpringBone(o);
+				Debug.Log($"Completed {o.name}");
+			}
+
+			Debug.Log("Optimize Completed.");
+		}
+
+		/// <summary>
 		/// XWearSource.assetを生成する
 		/// </summary>
-		[MenuItem("Assets/" + CreateXWearSourceMenuItemContext)]
+		[MenuItem(AssetContext + CreateXWearSourceMenuItemContext)]
 		public static void CreateMyScriptableObject()
 		{
 			// 既に存在する場合は警告を出しておく
